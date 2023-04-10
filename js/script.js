256bb479a1434f2f60a414706b1f9bc242cdd08d
DavSRod <davidsantiagorodriguezlosada@gmail.com>
2023-04-10 15:54:09 -0500

    add cambios

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-alert ("Especificaciones Hardware y Software")
+function comprobar(){
+    let nombre
+    nombre=document.getElementById("nombredelproducto").value
+    document.getElementById("respuesta1").innerHTML="<h3> Nombre del producto:"+nombre+"</h3>"
+}
